92580417fde3c451a3b3f873b82c6f202a5b41c0
waymaymay <Rdvickywei@gmail.com>
2024-01-08 19:21:11 +0800

    更新banner RWD-mobile、pc版

--- src/pages/home/home.js
+++ src/pages/home/home.js
@@ -1,16 +1,18 @@
-import React from 'react'
-import './home.css'
+import React from 'react';
+import './home.css';
 
 function Home(){
     const banner_img = require('../../images/banner_img.png')
+    const banner_h5 = require('../../images/H5-index-banner.png')
+
 
     return (
         <div className='layout'>
             <div className='banner_img'>
-                <img src= {banner_img} alt=" "/>
-                test
+                <img src= {banner_img} alt=" " className='banner web-banner'/>
+                <img src= {banner_h5} alt=' ' className='banner h5-banner'/>
             </div>
         </div>
-    )
+    );
 }
-export default Home
+export default Home;
