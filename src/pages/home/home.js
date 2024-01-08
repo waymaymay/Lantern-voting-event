2f9dc223fcbeae993f8e4a3c4562415795d0b5bd
waymaymay <Rdvickywei@gmail.com>
2024-01-08 14:40:46 +0800

    建立web-首頁banner cpmponent

---
+++ src/pages/home/home.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import './home.css'
+
+function Home(){
+    const banner_img = require('../../images/banner_img.png')
+
+    return (
+        <div className='layout'>
+            <div className='banner_img'>
+                <img src= {banner_img } />
+            </div>
+        </div>
+    )
+}
+export default Home
