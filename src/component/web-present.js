f0bde87986911a6bbf6020a27c26b72b86663d5e
Vicky Wei <cathsu@Vicky.local>
2024-01-12 18:00:17 +0800

    更新pc-index-活動辦法文案

---
+++ src/component/web-present.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import './web-present.css';
+
+function Web_present(){
+    return(
+        <div className='Web_colum'>
+            
+        </div>
+    )
+
+}
--- src/pages/home/home.js
+++ src/pages/home/home.js
@@ -5,12 +5,25 @@ function Home(){
     const banner_img = require('../../images/banner_img.png')
     const banner_h5 = require('../../images/H5-index-banner.png')
 
-
     return (
         <div className='layout'>
             <div className='banner_img'>
                 <img src= {banner_img} alt=" " className='banner web-banner'/>
                 <img src= {banner_h5} alt=' ' className='banner h5-banner'/>
+            </div>
+            <div className='main-content'>
+                <div className='notice-group'>
+                    <div className='notice-one'>
+                        <div className='index-top top'>活動時間</div>
+                        <div className='index-sub sub'>2025年1月20日</div>
+                    </div>
+
+                    <div className='notice-two'>
+                        <div className='index-top'>活動獎項</div>
+                        <div className='index-sub sub'>抽獎送台中名產</div>
+                    </div>
+                </div>
+                
             </div>
         </div>
     );
