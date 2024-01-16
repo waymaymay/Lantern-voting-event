d06de15e0d38e041165f5f3d706fbb727b9d4487
waymaymay <Rdvickywei@gmail.com>
2024-01-16 14:54:35 +0800

    更新upper button的component js

--- src/component/upperbutton/upperButton.js
+++ src/component/upperbutton/upperButton.js
@@ -1,30 +1,51 @@
-import React ,{useState} from "react";
+import React, { useState, useEffect } from "react";
 import '../upperbutton/upperButton.css';
 
 function UpperButton (){
     const upper_img = require('../../images/upper.png')
+    const [showButton, setShowButton] = useState(false);
 
     const buttonStyle={
         padding: '5px 5px;',
         border: 'none',
-        borderRadius: '8px',
         cursor: 'pointer',
+        position: 'fixed',
+        bottom: '20px', // 调整按钮在底部的距离
+        right: '20px', // 调整按钮在右侧的距离
+        display: showButton ? 'block' : 'none',
   };
 
+  const handleScrollTop= ()=>{
+    window.scrollTo({
+      top:0,
+      behavior:'smooth'
+    });
+  };
+
+  const handleScroll = () => {
+    // 监听页面滚动事件
+    if (window.scrollY > 200) {
+        setShowButton(true);
+    } else {
+        setShowButton(false);
+    }
+};
+
+useEffect (()=>{
+  // 添加页面滚动事件监听器
+  window.addEventListener('scroll', handleScroll);
+
+  // 在组件卸载时移除事件监听器
+  return () => {
+      window.removeEventListener('scroll', handleScroll);
+};
+},[]);
+
+
   return(
-    
-    <div className="upper_img">
+    <div className="upper_img" style={buttonStyle} onClick={handleScrollTop}>
       <img src={upper_img} alt='' className="upper_img"/>
-    
-    </div>
-    
-  );
+    </div>);
 }
 export default UpperButton;
 
-// return (
-//   <div className='layout'>
-//       <div className='banner_img'>
-//           <img src= {banner_img} alt=" " className='banner web-banner'/>
-//           <img src= {banner_h5} alt=' ' className='banner h5-banner'/>
-//       </div>
